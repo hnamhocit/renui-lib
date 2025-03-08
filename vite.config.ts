90510d743c6d0d78ebbe7505237006214f9de5b5
hnamhocit <hnamhocit@gmail.com>
2025-03-08 07:36:48 -0500

    Config build

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,30 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		dts({ rollupTypes: true, tsconfigPath: "./tsconfig.app.json" }),
+	],
 	build: {
 		lib: {
 			entry: resolve(__dirname, "lib/main.ts"),
 			name: "renui",
 			fileName: "renui",
+			formats: ["es", "cjs"], // Ensure both formats are included
 		},
 		rollupOptions: {
-			external: ["react", "react-rom", "react/jsx-runtime"],
+			external: ["react", "react-dom", "react/jsx-runtime"], // Fix typo: "react-rom" -> "react-dom"
 			output: {
 				globals: {
 					react: "React",
-					"react-rom": "ReactROM",
-					"react/jsx-runtime": "react/jsx-runtime",
+					"react-dom": "ReactDOM",
+					"react/jsx-runtime": "jsxRuntime",
 				},
 			},
 		},
