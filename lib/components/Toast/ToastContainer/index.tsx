115b187d39095b448255318eb17a89edd43d52cb
hnamhocit <hnamhocit@gmail.com>
2025-03-09 05:15:18 -0400

    fix nextjs document is not defined

--- lib/components/Toast/ToastContainer/index.tsx
+++ lib/components/Toast/ToastContainer/index.tsx
@@ -1,39 +1,13 @@
 "use client";
-import { useContext, useEffect, useRef } from "react";
-import { createPortal } from "react-dom";
+import { useContext } from "react";
 
 import Toast from "../";
 import { ToastContext } from "../../../context/ToastContext";
 
 const ToastContainer = () => {
 	const { toasts } = useContext(ToastContext);
-	const containerRef = useRef<HTMLDivElement | null>(null);
 
-	useEffect(() => {
-		// Create a div to hold the toasts
-		const container = document.createElement("div");
-		container.setAttribute("id", "__toast-container");
-		document.body.appendChild(container);
-		containerRef.current = container;
-
-		// Cleanup on unmount
-		return () => {
-			if (containerRef.current) {
-				document.body.removeChild(containerRef.current);
-			}
-		};
-	}, []);
-
-	if (!containerRef.current) return null;
-
-	return createPortal(
-		<>
-			{toasts.map((toast, index) => (
-				<Toast key={index} {...toast} />
-			))}
-		</>,
-		containerRef.current
-	);
+	return toasts.map((toast, index) => <Toast key={index} {...toast} />);
 };
 
 export default ToastContainer;
