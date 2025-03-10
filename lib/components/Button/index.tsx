11bdde52cc9d30d0e46b1dcba07777c4f25859ff
hnamhocit <hnamhocit@gmail.com>
2025-03-10 16:25:46 +0700

    add shadow for flat, solid, border button variant

--- lib/components/Button/index.tsx
+++ lib/components/Button/index.tsx
@@ -37,12 +37,15 @@ const iconSizeStyles: Record<Size, string> = {
 
 const variantStyles: Record<ButtonVariant, Record<Color, string>> = {
 	solid: {
-		default: "bg-gray-900 text-white hover:bg-gray-950",
-		primary: "bg-blue-600 text-white hover:bg-blue-700",
-		secondary: "bg-indigo-600 text-white hover:bg-indigo-700",
-		success: "bg-green-600 text-white hover:bg-green-700",
-		warning: "bg-amber-600 text-white hover:bg-amber-700",
-		danger: "bg-red-600 text-white hover:bg-red-700",
+		default: "bg-gray-900 text-white hover:bg-gray-950 shadow-gray-900/50",
+		primary: "bg-blue-600 text-white hover:bg-blue-700 shadow-blue-600/50",
+		secondary:
+			"bg-indigo-600 text-white hover:bg-indigo-700 shadow-indigo-600/50",
+		success:
+			"bg-green-600 text-white hover:bg-green-700 shadow-green-600/50",
+		warning:
+			"bg-amber-600 text-white hover:bg-amber-700 shadow-amber-600/50",
+		danger: "bg-red-600 text-white hover:bg-red-700 shadow-red-600/50",
 	},
 	light: {
 		default: "text-gray-900 hover:bg-gray-200",
@@ -54,24 +57,27 @@ const variantStyles: Record<ButtonVariant, Record<Color, string>> = {
 	},
 	border: {
 		default:
-			"border-2 border-gray-900 text-gray-900 hover:bg-gray-900 hover:text-white",
+			"border-2 border-gray-900 text-gray-900 hover:bg-gray-900 hover:text-white shadow-gray-900/50",
 		primary:
-			"border-2 border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white",
+			"border-2 border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white shadow-blue-600/50",
 		secondary:
-			"border-2 border-indigo-600 text-indigo-600 hover:bg-indigo-600 hover:text-white",
+			"border-2 border-indigo-600 text-indigo-600 hover:bg-indigo-600 hover:text-white shadow-indigo-600/50",
 		success:
-			"border-2 border-green-600 text-green-600 hover:bg-green-600 hover:text-white",
+			"border-2 border-green-600 text-green-600 hover:bg-green-600 hover:text-white shadow-green-600/50",
 		warning:
-			"border-2 border-amber-600 text-amber-600 hover:bg-amber-600 hover:text-white",
-		danger: "border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white",
+			"border-2 border-amber-600 text-amber-600 hover:bg-amber-600 hover:text-white shadow-amber-600/50",
+		danger: "border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white shadow-red-600/50",
 	},
 	flat: {
-		default: "bg-gray-600 text-white hover:bg-gray-700",
-		primary: "bg-blue-400 text-white hover:bg-blue-500",
-		secondary: "bg-indigo-400 text-white hover:bg-indigo-500",
-		success: "bg-green-400 text-white hover:bg-green-500",
-		warning: "bg-amber-400 text-white hover:bg-amber-500",
-		danger: "bg-red-400 text-white hover:bg-red-500",
+		default: "bg-gray-600 text-white hover:bg-gray-700 shadow-gray-600/50",
+		primary: "bg-blue-400 text-white hover:bg-blue-500 shadow-blue-400/50",
+		secondary:
+			"bg-indigo-400 text-white hover:bg-indigo-500 shadow-indigo-400/50",
+		success:
+			"bg-green-400 text-white hover:bg-green-500 shadow-green-400/50",
+		warning:
+			"bg-amber-400 text-white hover:bg-amber-500 shadow-warning-400/50",
+		danger: "bg-red-400 text-white hover:bg-red-500 shadow-red-400/50",
 	},
 };
 
